841a8f8eed60a710e0ad2498ba1a6f27a271312b
endrewnatta <endrew12natta@gmail.com>
2021-02-18 18:29:56 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,58 @@
+const help = (prefix) => {
+	return `
+
+     *INFO* 
+  *BOT EndrewMan*
+
+*=> Dono: Endrew*
+*=> wa.me/+554998304144
+*=> prefixo: ⫷ ${prefix} ⫸*
+
+*--> COMANDOS <--*
+
+*=> ${prefix}info*
+*=> ${prefix}blocklist*
+*=> ${prefix}setprefix*
+  
+*>>>>>>>>><<<<<<<<<*
+
+*=> ${prefix}figurinha*
+*=> ${prefix}sticker*
+*=> ${prefix}tfigurinha
+*=> ${prefix}tsticker
+*=> ${prefix}toimg*
+*=> ${prefix}wait*
+*=-=-=-=-=-=-=-=-=-=-=*
+*=> ${prefix}ytsearch*
+*=> ${prefix}tiktok*
+*=> ${prefix}tiktokstalk*
+*=> ${prefix}url2img*
+*=-=-=-=-=-=-=-=-=-=-=*
+*=> ${prefix}marcar*
+*=> ${prefix}marcar2*
+*=> ${prefix}marcar3*
+*=> ${prefix}clearall*
+*=> ${prefix}bc*
+*=> ${prefix}promover*
+*=> ${prefix}rebaixar*
+*=> ${prefix}add*
+*=> ${prefix}kick*
+*=> ${prefix}listadmins*
+*=> ${prefix}linkgroup*
+*=> ${prefix}leave*
+*=> ${prefix}welcome*
+*=> ${prefix}clone*
+*=-=-=-=-=-=-=-=-=-=-=*
+*=> ${prefix}simi*
+*=> ${prefix}simih*
+*=> ${prefix}matar pedro*
+*=-=-=-=-=-=-=-=-=-=-=*
+
+*-mais comandos em breve-*
+
+*n importa aonde nos vamos, nos sempre seremos nozes jaq nos n somos eles 😎✌✌*
+
+*>>>>>>>>>><<<<<<<<<<*`
+}
+
+exports.help = help
